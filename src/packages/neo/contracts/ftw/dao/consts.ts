3fb76920e9516bd20b2c1523454a64ecac9ff420
Ediopia <hieddiejung@gmail.com>
2022-09-07 14:16:50 -0700

    DAO testnet (T5) hash change

--- src/packages/neo/contracts/ftw/dao/consts.ts
+++ src/packages/neo/contracts/ftw/dao/consts.ts
@@ -2,6 +2,6 @@ import { CONST } from "../../../index";
 
 export const DAO_SCRIPT_HASH = {
   [CONST.PRIVATENET]: "",
-  [CONST.TESTNET]: "560e275f3a12e3150642bbba2c92c14caeece509",
+  [CONST.TESTNET]: "59abe08d5c815853b4ba1e21ad4c334820a067b9",
   [CONST.MAINNET]: "",
 };
