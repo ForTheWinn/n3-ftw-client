4de78c616ccc9dc06471efd62ed3f84fcd749f97
Ediopia <hieddiejung@gmail.com>
2024-07-30 23:57:34 -0700

    Update token contracts for testnet and mainnet

--- src/packages/evm/neox/tokens.ts
+++ src/packages/evm/neox/tokens.ts
@@ -1,5 +1,5 @@
-import { NEOX_MAINNET_CONTRACTS } from ".";
-import { FNEO_LOGO, MAINNET, NEO_LOGO, TESTNET } from "../../../consts/global";
+import { NEOX_MAINNET_CONTRACTS, NEOX_TESTNET_CONTRACTS } from ".";
+import { FNEO_LOGO, MAINNET, TESTNET } from "../../../consts/global";
 import { createTokenMetadata } from "../../../common/helpers";
 
 export const NEOX_MAINNET_TOKENS = [
@@ -34,34 +34,34 @@ export const NEOX_MAINNET_TOKENS = [
 
 export const NEOX_TESTNET_TOKENS = [
   createTokenMetadata({
-    hash: NEOX_MAINNET_CONTRACTS.NEP,
+    hash: NEOX_TESTNET_CONTRACTS.NEP,
     symbol: "NEP",
     icon: "/symbols/nep.png",
     decimals: 8,
     isWhitelisted: true,
   }),
   createTokenMetadata({
-    hash: NEOX_MAINNET_CONTRACTS.FNEO,
+    hash: NEOX_TESTNET_CONTRACTS.FNEO,
     symbol: "fNEO",
     icon: FNEO_LOGO,
     decimals: 8,
     isWhitelisted: true,
   }),
   createTokenMetadata({
-    hash: NEOX_MAINNET_CONTRACTS.GAS,
+    hash: NEOX_TESTNET_CONTRACTS.GAS,
     symbol: "GAS",
     icon: "/symbols/gas.svg",
     decimals: 18,
     isWhitelisted: true,
     isNative: true,
     nativePair: {
-      hash: NEOX_MAINNET_CONTRACTS.WGAS,
+      hash: NEOX_TESTNET_CONTRACTS.WGAS,
       symbol: "WGAS",
       decimals: 18,
     },
   }),
   createTokenMetadata({
-    hash: NEOX_MAINNET_CONTRACTS.WGAS,
+    hash: NEOX_TESTNET_CONTRACTS.WGAS,
     symbol: "WGAS",
     icon: "/symbols/gas.svg",
     decimals: 18,
--- src/packages/neo/contracts/ftw/swap/consts.ts
+++ src/packages/neo/contracts/ftw/swap/consts.ts
@@ -8,7 +8,5 @@ export const PRICE_IMPACT_LIMIT = 100;
 
 export const SWAP_SCRIPT_HASH = {
   [TESTNET]: "0xecb9465013477215e25aaf8e87ed02141ebe256e",
-  [MAINNET]: "0x997ced5777a3f66485d66828bda3864b8c8bdf95"
+  [MAINNET]: "0x997ced5777a3f66485d66828bda3864b8c8bdf95",
 };
-
-
